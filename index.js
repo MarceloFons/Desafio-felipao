7f39223c415038f6d24a882275759916f9559fc1
Marcelo Fonseca <134106147+MarceloFons@users.noreply.github.com>
2024-05-17 10:08:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ let moradia = " Castelo "
 let arma =" Espada "
 let armamento = " Canhão "
 const heroi = " Castor "
+var idade = " 33 "
+var crianca = idade - 30 + " anos";
+
+
+console.log("Lá nesse reino existe um salvador chamado" + heroi )
+console.log("Quando " + heroi + "chegou nesse reino, ele só tinha " + crianca)
+console.log("Um andarilho que só tem uma " + arma ) 
+console.log("O reino tem um " + armamento + " para sua defesa")
