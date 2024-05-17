72193d3d63c779b261dd694fabe23a1554dbe605
Marcelo Fonseca <134106147+MarceloFons@users.noreply.github.com>
2024-05-17 11:06:27 -0300

    Update index.js
    Projeto em fase média

--- index.js
+++ index.js
@@ -5,9 +5,45 @@ let armamento = " Canhão "
 const heroi = " Castor "
 var idade = " 33 "
 var crianca = idade - 30 + " anos";
+var menoridade = "Jovem";
+var maioridade = "Guerreiro";
+var adulto = maioridade >= menoridade;
 
 
 console.log("Lá nesse reino existe um salvador chamado" + heroi )
 console.log("Quando " + heroi + "chegou nesse reino, ele só tinha " + crianca)
 console.log("Um andarilho que só tem uma " + arma ) 
 console.log("O reino tem um " + armamento + " para sua defesa")
+console.log("Hoje" + heroi + maioridade + " após anos de luta ele chegou em um momento de reflexão.")
+
+
+console.log("Qual meu ponto de vida atual XP ?")
+
+//if,else if ,else
+let XP =  "10000"
+
+if(XP <= 1000){
+console.log(" XP Classe Ferro ")
+}
+else if(XP <= 2000){
+console.log("XP Classe Bronze")
+}
+else if(XP <= 6000){
+console.log("XP Classe Prata")
+}else if(XP <= 7000){
+console.log("XP Classe Ouro")
+}
+else if(XP <= 8000){
+console.log("XP Classe Platina diamante")
+}else if(XP <= 9000){
+console.log("XP Classe Ascendente")
+}
+else if(XP <= 10000){
+console.log("XP Classe Imortal")
+}
+else if(XP >= 10001){
+console.log("XP Classe Radiante")
+}
+else{
+console.log("Verifique o número inserido")
+  }
