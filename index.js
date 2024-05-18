a3e89cda81e3e6e5773be03e766fc893b270f887
Marcelo Fonseca <134106147+MarceloFons@users.noreply.github.com>
2024-05-18 09:14:39 -0300

    Projeto Simples concluído

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 console.log("Um reino chamado Fortis")
-let moradia = " Castelo "
+
+let moradia = "um Castelo, "
 let arma =" Espada "
 let armamento = " Canhão "
 const heroi = " Castor "
@@ -8,16 +9,16 @@ var crianca = idade - 30 + " anos";
 var menoridade = "Jovem";
 var maioridade = "Guerreiro";
 var adulto = maioridade >= menoridade;
-
-
+console.log("O reino tem " + moradia + "pequenas moradias, feiras, agricultura e um" + armamento + "para sua defesa")
 console.log("Lá nesse reino existe um salvador chamado" + heroi )
-console.log("Quando " + heroi + "chegou nesse reino, ele só tinha " + crianca)
+console.log("Quando" + heroi + "chegou nesse reino, ele só tinha " + crianca)
 console.log("Um andarilho que só tem uma " + arma ) 
-console.log("O reino tem um " + armamento + " para sua defesa")
 console.log("Hoje" + heroi + maioridade + " após anos de luta ele chegou em um momento de reflexão.")
-
-
+console.log("Aproveitou seu momento e foi a um 'sábio' ao leste de Fortis")
+console.log("Fez a seguinte pergunta: ")
 console.log("Qual meu ponto de vida atual XP ?")
+console.log("O 'sábio' afirmou que com base na sua experiência de batalha, na mente foi revelado um número de 10000")
+console.log("Por isso" + heroi + "está no nível de: " )
 
 //if,else if ,else
 let XP =  "10000"
@@ -39,7 +40,7 @@ console.log("XP Classe Platina diamante")
 console.log("XP Classe Ascendente")
 }
 else if(XP <= 10000){
-console.log("XP Classe Imortal")
+console.log("-XP Classe Imortal-")
 }
 else if(XP >= 10001){
 console.log("XP Classe Radiante")
